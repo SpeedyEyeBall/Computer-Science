e387ad3809135c9479b80cf574b821b3548273e8
SpeedyEyeBall <764138@student.derby-college.ac.uk>
2017-01-22 23:54:17 +0000

    New Assignment

--- Classwork/Week 9 Part 4.py
+++ Classwork/Week 9 Part 4.py
@@ -1,16 +1,16 @@
 #Lee-Roy Dobson Week 9 Work
 jellyBeans = raw_input('Input your jellybeans[r][g]: ')
-
+#Function takes 1 parameter, the string of beans.
 def checkaltern(beans):
-    prevbean = ''
-    for bean in beans:
-        if prevbean == '':
-            prevbean = bean
-        else:
-            if bean != prevbean:
-                prevbean = bean
+    prevbean = '' #variable for storing the previous bean.
+    for bean in beans:  #For loop runs through each character of the string
+        if prevbean == '': #checks if it's the first time looping.
+            prevbean = bean 
+        else: 
+            if bean != prevbean: #Is the current bean the same as the last?
+                prevbean = bean #Set the prevbean to the current bean
             else:
                 return(False)
-    return(True)
+    return(True) #Returns true if the for loop completes without returning false.
 
 print(checkaltern(jellyBeans))
